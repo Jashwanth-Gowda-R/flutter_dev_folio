7b7a2293a333c89e09163935a602f474ac0239b7
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2023-04-17 14:14:55 +0530

    fixed name issue in mobile view

--- lib/src/home/my_name.dart
+++ lib/src/home/my_name.dart
@@ -16,35 +16,35 @@ class MyName extends StatelessWidget {
   Widget build(BuildContext context) {
     final double width = MediaQuery.of(context).size.width;
     return Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 12.0),
-        child: isMobile
-            ? Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: List.generate(data.split(' ').length, (int i) {
-                  return SizedBox(
-                    width: width - width * 0.4,
-                    child: FittedBox(
-                      fit: BoxFit.cover,
-                      child: Text(
-                        data.split(' ')[i],
-                        textScaleFactor: 4.5,
-                        style: TextStyle(
-                          fontFamily: 'FjallaOne',
-                          // letterSpacing: 10.5,
-                          fontWeight: FontWeight.w500,
-                          color: Theme.of(context).primaryColor,
-                        ),
-                      ),
+      padding: const EdgeInsets.symmetric(horizontal: 12.0),
+      child: isMobile
+          ? Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: List.generate(data.split(' ').length, (int i) {
+                return SizedBox(
+                  width: width - width * 0.3,
+                  child: Text(
+                    data.split(' ')[i],
+                    textScaleFactor: 4.5,
+                    style: TextStyle(
+                      fontFamily: 'FjallaOne',
+                      // letterSpacing: 10.5,
+                      fontWeight: FontWeight.w500,
+                      color: Theme.of(context).primaryColor,
                     ),
-                  );
-                }))
-            : Text(data,
-                textScaleFactor: 7,
-                style: TextStyle(
-                  fontFamily: 'FjallaOne',
-                  letterSpacing: 20.5,
-                  color: Theme.of(context).primaryColor,
-                  fontWeight: FontWeight.w500,
-                )));
+                  ),
+                );
+              }))
+          : Text(
+              data,
+              textScaleFactor: 7,
+              style: TextStyle(
+                fontFamily: 'FjallaOne',
+                letterSpacing: 20.5,
+                color: Theme.of(context).primaryColor,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+    );
   }
 }
